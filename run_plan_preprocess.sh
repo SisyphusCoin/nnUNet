a5c1e52a4086e109a0d762d6d382524ea7733af4
Jinqian Wang <wang@jinqian.tech>
2025-10-03 15:20:19 +0930

    feat: remove the scripts, modify the pyproject.toml

--- run_plan_preprocess.sh
+++
@@ -1,7 +0,0 @@
-time=$(date "+%Y%m%d-%H%M%S%3N")
-name=${time}
-dataset=114
-
-mkdir -p ./logs/${dataset}
-
-nnUNetv2_plan_and_preprocess -d ${dataset} --verify_dataset_integrity > ./logs/${dataset}/${name}_plan.log 2>&1 &
